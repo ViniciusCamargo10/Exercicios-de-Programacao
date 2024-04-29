56650ab11167f44162f8ccbc10d6114fe7080440
ViniciusCamargo10 <66333217+ViniciusCamargo10@users.noreply.github.com>
2024-04-29 10:24:14 -0300

    Add files via upload

---
+++ Python - aula 1 ( variavel).py
@@ -0,0 +1,17 @@
+"""
+str = string -> textos
+int = inteiro -> numeros inteiros
+float = numeros decimais 
+bool = verdadeiro ou falso 
+list = listas de numeros, nomes , etc 
+
+"""
+print(type("Vinicius"))
+print(type(5))
+print(type(5.23))
+print(type(True))
+
+letras = ["A", "b", "c"]
+print(type(letras))
+
+print("Vinicius", type("Vinicius"))
